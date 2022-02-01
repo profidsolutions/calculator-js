25126052c61709d6a860917f75baff8893fa57c2
Fidel <fidel.providence@cgi.com>
2022-02-01 09:59:05 -0500

    Add jenkins file

--- src/calc.js
+++ src/calc.js
@@ -1,3 +1,11 @@
 const sum = (a, b) => a + b; //created a function
 
+
+//below block of code is same as above
+/*
+function sum(a,b) {
+    return a + b;
+    
+}*/
+
 export {sum}; //export the fucntion some out of this file so it can be used elsewhere
