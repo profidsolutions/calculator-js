f483766322026229b7d09186f50b5d9d0013e076
Fidel <fidel.providence@cgi.com>
2022-02-01 10:12:22 -0500

    modified calc.test.js added a new variable expecting 10

--- src/calc.test.js
+++ src/calc.test.js
@@ -1,5 +1,5 @@
 import { sum } from "./calc";
 
-test (' Adding 2 + 3 should give me 5', ( ) => {
-    expect(sum(2, 3)).toBe(10);
+test (' Adding 5 + 5 should give me 10', ( ) => {
+    expect(sum(5, 5)).toBe(10);
 })
